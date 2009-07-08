c440745612ea6e4145e3b2b7c1291b93107e1153
Jason Soo <wwwjscom@gmail.com>
2009-07-08 00:30:01 -0500

    added new exam questions

--- db/migrate/20090610051341_create_exams.rb
+++ db/migrate/20090610051341_create_exams.rb
@@ -6,7 +6,7 @@ class CreateExams < ActiveRecord::Migration
 
       t.timestamps
     end
-    Exam.create(:name => 'M_0-5', :time => 1)
+    Exam.create(:name => 'M_0-50', :time => 11)
   end
 
   def self.down
