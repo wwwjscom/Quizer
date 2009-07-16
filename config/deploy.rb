fefa4026d1876c3a4b707b11bf4267604915c5b8
Jason Soo <wwwjscom@gmail.com>
2009-07-15 19:52:41 -0500

    new deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,52 @@
-set :application, "quizer"
-set :repository,  "git@github.com:wwwjscom/Quizer.git"
-set :scm, :git
-set :deploy_to, '/www/quizer'
-set :runner, 'root'
-set :branch, "dev"
+# Please install the Engine Yard Capistrano gem
+# gem install eycap --source http://gems.engineyard.com
+require "eycap/recipes"
+
+set :keep_releases, 5
+set :application,   'quizer'
+set :repository,    'git@github.com:wwwjscom/Quizer.git'
+set :deploy_to,     "/data/#{application}"
+set :deploy_via,    :export
+set :monit_group,   "#{application}"
+set :scm,           :git
+set :git_enable_submodules, 1
+# This is the same database name for all environments
+set :production_database,'quizer_production'
+
+set :environment_host, 'localhost'
+set :deploy_via, :remote_cache
+
+# uncomment the following to have a database backup done before every migration
+# before "deploy:migrate", "db:dump"
+
+# comment out if it gives you trouble. newest net/ssh needs this set.
+ssh_options[:paranoid] = false
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true # required for svn+ssh:// andf git:// sometimes
+
+# This will execute the Git revision parsing on the *remote* server rather than locally
+set :real_revision, 			lambda { source.query_revision(revision) { |cmd| capture(cmd) } }
 
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+task :production do
+  role :web, '174.129.10.160'
+  role :app, '174.129.10.160'
+  role :db, '174.129.10.160', :primary => true
+  set :environment_database, Proc.new { production_database }
+  set :dbuser,        'deploy'
+  set :dbpass,        'lgffQVpnfg'
+  set :user,          'deploy'
+  set :password,      'lgffQVpnfg'
+  set :runner,        'deploy'
+  set :rails_env,     'production'
+end
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
 
-server "vividsounds", :app, :web, :db, :primary => true
+# TASKS
+# Don't change unless you know what you are doing!
+
+after "deploy", "deploy:cleanup"
+after "deploy:migrations", "deploy:cleanup"
+after "deploy:update_code","deploy:symlink_configs"
+
